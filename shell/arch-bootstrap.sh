90b383fc9e00ab69efc1ef7e9069025249adbd0b
Nicholas Scheurich <nick@scheurich.me>
2015-11-19 11:43:22 -0600

    Add empty .zshrc

--- shell/arch-bootstrap.sh
+++ shell/arch-bootstrap.sh
@@ -3,3 +3,5 @@
 pacman --sync --refresh --sysupgrade --noconfirm
 pacman --sync --noconfirm git vim zsh tmux base-devel
 chsh vagrant -s `which zsh`
+touch /home/vagrant/.zshrc
+chown -R vagrant:users /home/vagrant/.zshrc
