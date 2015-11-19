73f8ab8689c6e89448f703717b2231c84fc8f215
Nicholas Scheurich <nick@scheurich.me>
2015-11-19 11:44:15 -0600

    Install ruby-build

--- shell/rbenv.sh
+++ shell/rbenv.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-HOME=/home/vagrant
-
-git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.zshrc
-git clone https://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(rbenv init -)"' >> ~/.zshrc
+source ~/.zshrc
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 if [ -n "$1" ]; then
   rbenv install $1
   rbenv global $1
